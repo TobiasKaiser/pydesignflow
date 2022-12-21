1576cbae4fb741c27523054099739c8ab83dbea9
Tobias Kaiser <kaiser@tu-berlin.de>
2022-12-21 11:58:35 +0100

    FileCollection improvement

--- pydesignflow/filemgmt.py
+++ pydesignflow/filemgmt.py
@@ -28,13 +28,39 @@ class FileCollectionItem:
     path: Path
     attrs: dict[str, object]
 
+    def __iter__(self):
+        return iter((self.path, self.attrs))
+
 class FileCollection:
     """
     A collection of files of the same type with different attributes such as voltage, process corner, temperature etc.
     """
 
-    def __init__(self):
-        self.items = []
+    def __init__(self, items: list[FileCollectionItem]=None):
+        if items == None:
+            items = []
+        self.items = items
+
+    def __add__(self, other):
+        return FileCollection(self.items + other.items)
+
+    def __len__(self):
+        return len(self.items)
+
+    def __getitem__(self, key):
+        return self.items[key]
+
+    def __iter__(self):
+        return iter(self.items)
+
+    def tag(self, **attrs: dict[str, object]):
+        """
+        Returns copy of the FileCollection with passed attrs added to all elements.
+        """
+        ret = FileCollection()
+        for i in self.items:
+            ret.items.append(FileCollectionItem(i.path, i.attrs | attrs))
+        return ret
 
     def add(self, path: Path, **attrs: dict[str, object]):
         """
@@ -45,44 +71,42 @@ class FileCollection:
     def __repr__(self):
         return f"FileCollection:{self.items}"
 
-    def all(self, **filters: dict[str, object]):
+    def filter(self, missing_key_deselects: bool = False, **filters: dict[str, object]):
         """
-        Returns iterator over all elements that match **filters.
+        Returns FileCollection with all files that match given filters.
         """
-        for itm in self.items:
-            selected = True
+        
+        def filter_func(item):
             for key, value in filters.items():
                 try:
-                    if itm.attrs[key] != value:
-                        selected = False
+                    if item.attrs[key] != value:
+                        return False
                 except KeyError:
-                    selected = False
-            if selected:
-                yield itm.path
+                    if missing_key_deselects:
+                        return False
+            return True
+        
+        return FileCollection(list(filter(filter_func, self.items)))
 
-    def one(self, filter_dict={}, **filters: dict[str, object]):
+    def one(self, missing_key_deselects: bool = False, **filters: dict[str, object]):
         """
-        Returns element that matches **filters. Alternatively, a filter dict can be passed as single argument.
+        Returns element that matches **filters.
 
         Raises FileManagementError if **filters are ambiguous (multiple matches) or when no match is found.
         """
-        filters |= filter_dict
-        res = list(self.all(**filters))
+        res = self.filter(missing_key_deselects=missing_key_deselects, **filters)
         if len(res) < 1:
             raise FileManagementError(f"No result for FileCollection filters {filters}.")
         elif len(res) > 1:
             raise FileManagementError(f"Ambiguous result for FileCollection filters {filters}.")
         else:
-            return res[0]
+            return res[0].path
 
-    def __call__(self, *args, **filters):
+    def __call__(self, *args, **kwargs):
         """
         Alias for .one()
         """
-        return self.one(*args, **filters)
-
-    def __len__(self):
-        return len(self.items)
+        return self.one(*args, **kwargs)
 
     @classmethod
     def frompattern(cls, dir, pattern, decoder):
